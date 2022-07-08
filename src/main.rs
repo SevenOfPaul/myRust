1b98c7184df7abee12da75f8abd26e4046c73fbd
paul <13136918273@gmail.com>
2022-07-08 20:24:59 +0800

    类型

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,10 @@ fn main() {
     let mut a:u32=1; 
     let c:char='你';
     let d:char='好';
+    let e:[char;3]=['你','好','吗'];
     if b{
         println!("Hello, rust!{}&{}",a,MAX_POINT);
     }
     a=3;
-    print!("Hello, rust!{},{}{}",a,c,&d);
+    print!("Hello, rust!{},{}{}&{}\n{}{}{}?",a,c,d,isize::max_value(),e[0],e[1],e[2]);
 }
