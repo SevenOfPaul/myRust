5fbc5942348290a2a3f54ffb6afa40302eb2d825
paul <13136918273@gmail.com>
2022-07-11 19:48:12 +0800

    new

--- rust.md
+++ rust.md
@@ -92,3 +92,17 @@ while i!=10{
  ```
  存储在2022-7-8
  ```
+ ## 函数 ##
+1. 声明函数 `fn name(){}`
+```rust
+fn test(number:i32)->i32{
+ print!("你好{}",number);
+ return number+1
+}
+fn main(){
+    let mut a:i32=16;
+    let b:i32=test(a);
+    print!("{}",b)
+}
+
+ ```
--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,9 @@
-const MAX_POINT:u32=10000;
-fn main() {
-    let b:bool=true;
-    let mut a:u32=1; 
-    let c:char='你';
-    let d:char='好';
-    let e:[char;3]=['你','好','吗'];
-    let t:(u32,i32,char)=(12,-12,'t');
-    let condition=true;
-    if b{
-        println!("Hello, rust!{}&{}",a,MAX_POINT);
-    }
-    a=3;
-    let number=if condition {
-        5
-    } else {
-        6
-    };
-    println!("Hello, rust!{},{}{}&{}",a,c,d,isize::max_value());
-    for i in &e{
-          print!("{}",i)
-       
-    }
-    for i in 0..10{
-        println!("{}",i);
-    }
-    print!("?{}{}{},{}",t.0,t.1,t.2,number);
-    let f=loop{
-        a+=1;
-        print!("{}",a);
-        if a>=100{
-            break a;
-        }
-       };
-       print!("{}",f);
+fn test(number:i32)->i32{
+ print!("你好{}",number);
+ return number+1
+}
+fn main(){
+    let mut a:i32=16;
+    let b:i32=test(a);
+    print!("{}",b)
 }
