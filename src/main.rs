a5fa266f8c7683c3e00db44a75eba357b542a9b8
paul <13136918273@gmail.com>
2022-07-13 18:51:52 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,15 @@
-fn test(a:isize)->isize{
+fn test(a:&mut isize)->isize{
  print!("你好{}",a);
-  a+1
+  *a+=1;
+  return *a;
 }
 fn main(){
-    let a:isize=16;
-    let b:isize=test(a);
+    let mut a:isize=16;
+    let b:isize=test(&mut a);
     let mut s:String=String::from("字符串");
     s.push_str("你好吗");
     let s2=s.clone();
     let s = String::from("hello");  // s 进入作用域
-    print!("b={}&a={}&s2={}",b,s,s2)
+    let s3=&s;
+    print!("b={}&a={}&s2={}s3={}",b,a,s2,*s3)
 }
