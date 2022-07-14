7cdd235a51dc9449bf5a16c044c849476b34f79e
paul <13136918273@gmail.com>
2022-07-14 18:20:36 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,6 @@ fn main(){
  let three=test(&mut two);
  let s=String::from("hello world!");
   let s2=&s[0..s.len()];
-  //在对字符串使用切片语法时需要格外小心，切片的索引必须落在字符之间的边界位置，
-  //也就是 UTF-8 字符的边界，例如中文在 UTF-8 中占用三个字节,下面的代码就会崩溃:
   let hw:[char;5]=['h','e','l','l','o'];
   let  mut wh:&[char]=&hw[0..=hw.len()-1];
   print!("\ntwo={}\nthree={}\ns2={}",two,three,s2);
