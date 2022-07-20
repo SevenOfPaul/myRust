ae63183e53465662630889788b5ec8f9fed9ef08
paul <13136918273@gmail.com>
2022-07-20 18:54:17 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,21 @@
-
-fn main() {
-    
-   }
+use std::fs::File;
+use std::io::{prelude::*, Error};
+fn read_file(path:String)->Result<String,Error>{
+    let mut file = File::open(path)?;
+    let mut s=String::new();
+     file.read_to_string(&mut s)?;
+     Ok(s)
+}
+fn main(){
+    // let mut file = File::open("../test.txt");
+    // let mut content = String::new();
+    // match file{
+    //     Ok(mut file) => file.read_to_string(&mut content).unwrap(),
+    //     Err(_) =>panic!("出错了")
+    // };
+    let mut content = read_file("../test.txt".to_string());
+    match &content {
+     Ok(content)=>  println!("{:?}",content),
+     Err(e) => panic!("{}", e)
+    }
+}
--- target/debug/rust.d
+++ target/debug/rust.d
@@ -1,1 +1,1 @@
-C:\Users\Paul\OneDrive\桌面\nest\rust\target\debug\rust.exe: C:\Users\Paul\OneDrive\桌面\nest\rust\src\main.rs C:\Users\Paul\OneDrive\桌面\nest\rust\src\newlib\src\A.rs C:\Users\Paul\OneDrive\桌面\nest\rust\src\newlib\src\factory.rs C:\Users\Paul\OneDrive\桌面\nest\rust\src\newlib\src\lib.rs
+C:\Users\Paul\OneDrive\桌面\nest\rust\target\debug\rust.exe: C:\Users\Paul\OneDrive\桌面\nest\rust\src\main.rs
