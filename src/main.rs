065bea18bd03140c8501a6f72b0754b61ec6bce8
paul <13136918273@gmail.com>
2022-07-20 13:39:37 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,4 @@
-use newlib::factory as F;
-use newlib::A;
-use rand::prelude::*;
-//这个可以忽略
-// extern crate crypto;
-use crypto::digest::Digest;
-use crypto::sha3::Sha3;
+
 fn main() {
-    let x: u8 = random();
-    F::produce_refrigerator::produce_re();
-    F::produce_washing_machine::produce_re();
-    let p=A::Person::new_person(String::from("张三"),16);
-    let mut hahser=Sha3::sha3_256();
-    hahser.input_str("hello world!");
-    let result=hahser.result_str();
-    println!("{:?}\n{:?}",p,result);
-   println!("main{}",x);
+    
    }
--- src/newlib/target/debug/deps/newlib-c71ae8e7f0df7487.d
+++
@@ -1,7 +0,0 @@
-c:\Users\Paul\OneDrive\桌面\nest\rust\src\newlib\target\debug\deps\newlib-c71ae8e7f0df7487.rmeta: src\lib.rs src\factory.rs src\A.rs
-
-c:\Users\Paul\OneDrive\桌面\nest\rust\src\newlib\target\debug\deps\newlib-c71ae8e7f0df7487.d: src\lib.rs src\factory.rs src\A.rs
-
-src\lib.rs:
-src\factory.rs:
-src\A.rs:
