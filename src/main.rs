fd93afdcccc3a058855cf6190f649ff35b5f9f0b
paul <13136918273@gmail.com>
2022-08-02 20:25:36 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -1,81 +1,16 @@
-struct Counter {
-    count: i32,
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
 }
-impl Counter {
-    fn new() -> Counter {
-        Counter { count: 0 }
-    }
-}
-impl Iterator for Counter {
-    type Item = i32;
-    fn next(&mut self) -> Option<Self::Item> {
-        self.count += 1;
-        if self.count < 6 {
-            Some(self.count)
-        } else {
-            None
-        }
-    }
-}
-// #[derive(Debug)]
-// struct NumberArray<'a> {
-//     count: &'a [i32],
-//     index:i32
-// }
-// impl NumberArray<'_>{
-//  fn new(arr:[i32;5])->NumberArray<'static>{
-//    let mut result=&arr[0..arr.len()-1];
-//    NumberArray{count:result,index:0}
-//  }
-// }
-// impl<'a> Iterator for NumberArray<'_>{
-//     type Item = &'a[i32];
-//     fn next(&mut self)->Option<Self::Item >{
-//         self.index += 1;
-//         let index=self.index;
-//          if self.index<=self.count.len() as i32 {
-//           return Some(self.count.clone())
-//          }else{
-//             return None;
-//          }
-//     }
-// }
+
 fn main() {
-    ///声明动态数组
-    let v1 = vec![1, 2, 3];
-    let mut v1_iter = v1.iter();
-    match v1_iter.next() {
-        Some(v) => println!("{}", v),
-        None => todo!(),
-    }
-    match v1_iter.next() {
-        Some(v) => println!("{}", v),
-        None => todo!(),
-    }
-    match v1_iter.next() {
-        Some(v) => println!("{}", v),
-        None => todo!(),
-    }
-    let total: i32 = v1.iter().sum();
-    let v2: Vec<_> = v1.iter().map(|x| x).collect();
-    let mut counter=Counter::new();
-    for i in (0..6)  {
-        if let Some(v)=counter.next(){
-            println!("{:?}",v);
-        }else{
-            println!("结束了")
-        }
-    }
-    println!("{:?}", total);
-    println!("{:?}", v2);
-    println!("hello world");
-    // let na=NumberArray::new([1,2,3,4,5].to_owned());
-    // println!("数组{:?}",&na);
-    // for n in na{
-    //     println!("数组{:?}",n);
-    // }
-    let total: i32 = v1_iter.sum();
-    println!("{:?}",total);
-     let total2:Vec<_>=v1.iter().map(|x|x).collect();
-     println!("{:?}",total2);
+    //b存储在栈上 5存储在堆上 b指向5
+    let b = Box::new(12);
+    use List::Cons;
+    use List::Nil;
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    println!("{:?}&{:?}", b, list);
+    println!("{:?}&", b);
 }
