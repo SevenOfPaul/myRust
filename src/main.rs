57013f6c1b19fe03e9d66e1d1486af8b398b8e3e
paul <13136918273@gmail.com>
2022-08-12 20:47:08 +0800

    NEW

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,13 @@
-use std::{sync::{Mutex, Arc}, thread, rc::Rc};
+use std::path::Components;
 
-
+use gui::{Screen,Button,selectBox};
 fn main() {
- let mut m=Arc::new(Mutex::new(5));
- let mut handles=Vec::new();
- for n in 0..10{
-    let mut counter=Arc::clone(&m);
-  let handle=thread::spawn(move ||{
-        let mut num=counter.lock().unwrap();
-         *num+=1;
-    });
-    handles.push(handle);
- }
- for handle in handles{
-    handle.join().unwrap();
- }
-
- println!("{:?}",m);
+   let mut s=Screen{
+      components:vec![]
+   };
+   let b1=Button{width:200,height:200,label:String::from("确认")};
+   let b2=Button{width:200,height:200,label:String::from("取消")};
+   s.components.push(Box::new(b1));
+   s.components.push(Box::new(b2));
+   s.run();
 }
