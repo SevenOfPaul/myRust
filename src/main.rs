32e81ce47081383eac89b73cff870dd2aec400be
paul <13136918273@gmail.com>
2022-08-21 21:00:40 +0800

    new

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,12 @@
-use std::process::Command;
-#[derive(Debug)]
-enum test{
-  name
-}
-extern "C"{
-  fn abs(input:i32)->i32;
-  fn rand()->i32;
-}
-static mut HELLO_WORLD:&str="hello";
-fn change_static(){
-  unsafe{
-    HELLO_WORLD="hello world!";
-  }
-}
+type MyI32=i8;
+use std::result;
+use std::io::Error;
+type Result<T>=result::Result<T,Error>;
 fn main() {
-  let mut num=5;
-  unsafe {
-    let r1:*const i32=&num ;
-    let str="string";
-    println!("{}", str.as_ptr() as u8);
-    let c1=r1.clone();
-    let r2:*const u8=str.as_ptr();
-    println!("{}",*r2);
-    // Command::new("cmd").arg("/c").arg("").output().expect("cmd exec error!");
-   println!("{}__{}",abs(12),rand());
-    change_static();
-    println!("{:?}__{}",*r1,HELLO_WORLD);
-    println!("{:?}",test::name)
-}
+let  a=16;
+let b:MyI32=17;
+// if let i=b{
+//    println!("{}",a+b);
+// }
+println!("{}",a+b);
 }
