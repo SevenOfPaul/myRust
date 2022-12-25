396261243952b8d8d3a1be87739ce9da8077fbc2
paul <13136918273@gmail.com>
2022-12-25 11:03:38 +0800

    map

--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,14 @@
-use std::borrow::BorrowMut;
-use std::cell::RefCell;
-use std::cell::Ref;
-#[derive(Default)]
-struct Trie {
-    pas:i32,
-    end:i32,
-    nexts:[Option<Box<Trie>>;26]
-}
-impl Trie {
-
-    fn new() -> Self {
-        Trie::default()
-    }
-    fn insert(&mut self, word: String) {
-        let mut node=self;
-        //要点1
-      for s in word.bytes(){
-          //要点2
-          let index=(s - b'a') as usize;
-          //要点3
-       if node.nexts[index].is_none(){
-           //不存在
-           node.nexts[index]=Some(Box::new(Trie::new()));
-           //要点4
-           node=node.nexts[index].as_deref_mut().unwrap();
-           node.pas+=1;
-       }else{
-     //存在
-           node=node.nexts[index].as_deref_mut().unwrap();
-           node.pas+=1;
-       }
-      }
-        node.end+=1;
-    }
-
-    fn search(& self, word: String) -> bool {
-        let mut node=self;
-        for s in word.bytes(){
-            let index=(s-b'a') as usize;
-            if !node.nexts[index].is_none(){
-                node= node.nexts[index].as_deref().unwrap();
-            }else{
-                return false
-            }
-        }
-       if node.end!=0{
-           return true
-       }else{
-           return false
-       }
-    }
-
-    fn starts_with(&self, prefix: String) -> bool {
-        let mut node=self;
-        for s in prefix.bytes(){
-            let index=(s-b'a') as usize;
-            if !node.nexts[index].is_none(){
-                node= node.nexts[index].as_deref().unwrap();
-            }else{
-                return false
-            }
-        }
-        return true
-    }
-}
 
 fn main(){
-
+ let a=0;
+    let mut arr:Vec<i32>=Vec::with_capacity(10);
+    //假定想要获取第178个bit的状态
+    let numIndex=178/32;
+    let bitIndex=178%32;
+    //取得i位的状态
+    let s=(arr[numIndex]>>bitIndex)&1;
+   //修改状态为1
+    arr[numIndex]=arr[numIndex]|(1<<bitIndex);
+    //修改状态为0
+    arr[numIndex]=arr[numIndex]&(!(1<<bitIndex));
 }
